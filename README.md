88e609c071b593b7d20cc0920a6030fbf84b0a57
TOMAS VERGARA <tomvergara@uc.cl>
2020-12-23 14:59:44 -0300

    Initial commit!

---
+++ README.md
@@ -0,0 +1,12 @@
+TwitchBot for MaginiGhost
+
+Commands:
+
+```!hora```: gives back time in Chile.
+
+```!random <number>```: gives back a random number from 1 to \<number\>.
+
+
+Details:
+
+Must download Node.js and execute ```npm install tmi.js --save``` in command window in twitchbot folder.
---
+++ index.js
@@ -0,0 +1,43 @@
+const tmi = require('tmi.js');
+const now = new Date();
+
+const options = {
+    options: {
+        debug: true,
+    },
+    connection: {
+        cluster: 'aws',
+        reconnect: true,
+    },
+    identity: {
+        username: 'twitchbot_test',
+        password: 'oauth:ieezow93nwkb6ptjq12mx5eopyp51s',
+    },
+    channels: ['MaginiGhost'],
+};
+
+
+const client = new tmi.client(options);
+
+client.connect();
+
+
+client.on('connected', (address, port) => {
+    client.action('MaginiGhost', 'Hola chat, el bot está conectado :)');
+});
+
+
+client.on('chat', (channel, user, message, self) => {
+    if (message === '!hora') {
+        client.action('MaginiGhost', now);
+    }
+
+    if (message.split(' ')[0] === '!random') {
+        if (message.split(' ').length === 2) {
+            var number = parseInt(message.split(' ')[1]);
+            var result = Math.floor(Math.random() * number) + 1;
+            client.action('MaginiGhost', result.toString());
+
+        }
+    }
+})
